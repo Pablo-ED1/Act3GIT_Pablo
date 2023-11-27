ba0cc745b896c85c276d912eb6ac251373302c80
Pablo-ED1 <a_pablo.aguilera.salinas@iespablopicasso.es>
2023-11-27 08:51:45 +0100

    Cambio 30 por 20

--- Act3GIT/src/NumerosAleatorios.java
+++ Act3GIT/src/NumerosAleatorios.java
@@ -8,9 +8,9 @@ public class NumerosAleatorios {
     public static void mostrarNumerosAleatorios() {
         Random random = new Random();
 
-        System.out.println("20 números aleatorios entre 1 y 10:");
+        System.out.println("30 números aleatorios entre 1 y 10:");
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 30; i++) {
             int numeroAleatorio = random.nextInt(10) + 1;
             System.out.println(numeroAleatorio);
         }
